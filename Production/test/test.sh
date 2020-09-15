a37eec3a328134fe51d1ab104465d7443e121c33
Karri DiPetrillo <kdipetri@github.com>
2020-09-14 19:11:08 -0500

    turning on splitting for uproot

--- Production/test/test.sh
+++ Production/test/test.sh
@@ -1,13 +1,13 @@
 # Signal
-  #cmsRun \
-  #/uscms_data/d3/kdipetri/SUEP_10_2_21_treeMaker/CMSSW_10_2_21/src/TreeMaker/Production/test/runMakeTreeFromMiniAOD_cfg.py \
-  #scenario=Autumn18sig \
-  #inputFilesConfig=PrivateSamples.SUEP_2018_mMed-750_mDark-2_temp-2_decay-generic_13TeV-pythia8_n-100 \
-  #nstart=0 \
-  #nfiles=10 \
-  #outfile=Autumn18.SUEP_2018_mMed-750_mDark-2_temp-2_decay-generic_13TeV-pythia8_n-100 \
-  #numevents=100 \
-  #>& Autumn18.SUEP_2018_mMed-750_mDark-2_temp-2_decay-generic_13TeV-pythia8_n-100.log &
+  cmsRun \
+  /uscms_data/d3/kdipetri/SUEP_10_2_21_treeMaker/CMSSW_10_2_21/src/TreeMaker/Production/test/runMakeTreeFromMiniAOD_cfg.py \
+  scenario=Autumn18sig \
+  inputFilesConfig=PrivateSamples.SUEP_2018_mMed-750_mDark-2_temp-2_decay-darkPho_13TeV-pythia8_n-100 \
+  nstart=0 \
+  nfiles=10 \
+  outfile=Autumn18.SUEP_2018_mMed-750_mDark-2_temp-2_decay-darkPho_13TeV-pythia8_n-100 \
+  numevents=100 \
+  >& Autumn18.SUEP_2018_mMed-750_mDark-2_temp-2_decay-darkPho_13TeV-pythia8_n-100.log &
 # QCD 
   #cmsRun \
   #/uscms_data/d3/kdipetri/SUEP_10_2_21_treeMaker/CMSSW_10_2_21/src/TreeMaker/Production/test/runMakeTreeFromMiniAOD_cfg.py \
@@ -29,4 +29,4 @@
   ##numevents=1000 \
   #>& Run2018D-PromptReco-v2.JetHT.log &
 
-python test_qcd.py
+#python test_qcd.py
